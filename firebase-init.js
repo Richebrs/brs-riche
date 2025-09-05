740561788a884673018731069f371ea023ee2c9e
Richebrs <richebrs6@gmail.com>
2025-09-05 13:28:38 +0100

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,8 +1,8 @@
+
 // Fichier : firebase-init.js
 // Configuration Firebase pour ton projet BRS Riche
 
-// Importé depuis les scripts Firebase dans index.html
-// Assure-toi que ces scripts sont chargés avant app.js
+// Assure-toi que dans tes pages HTML tu as bien ajouté :
 // <script src="https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js"></script>
 // <script src="https://www.gstatic.com/firebasejs/10.12.0/firebase-auth-compat.js"></script>
 // <script src="https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore-compat.js"></script>
@@ -11,7 +11,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDQ2UWK115OXfThwaGfetIUSPPz_Ry78BA",
   authDomain: "brs-riche.firebaseapp.com",
   projectId: "brs-riche",
-  storageBucket: "brs-riche.firebasestorage.app",
+  storageBucket: "brs-riche.appspot.com",   // ✅ corrigé
   messagingSenderId: "44338076148",
   appId: "1:44338076148:web:b0c32d6caa12221fe601a1",
   measurementId: "G-FQ5ZFB4FYM"
@@ -22,6 +22,21 @@ if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-// Exporter db et auth pour usage dans app.js
+// Exporter db et auth
 const db = firebase.firestore();
 const auth = firebase.auth();
+
+// Fonction utilitaire pour écouter en temps réel
+function listenUserData(callback) {
+  auth.onAuthStateChanged(user => {
+    if (user) {
+      db.collection("users").doc(user.uid).onSnapshot(doc => {
+        if (doc.exists) {
+          callback(user, doc.data());
+        }
+      });
+    } else {
+      window.location.href = "login.html";
+    }
+  });
+}
